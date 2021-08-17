0aa9959146e1e56fb0b03fbd020af19afffae4bd
JDMPasquel <jdamupasquel@unicauca.edu.co>
2021-08-17 18:11:06 -0500

    Adding an equals and ToString methods

--- Component-service/src/main/java/co/edu/unicauca/componentservice/domain/entity/Component.java
+++ Component-service/src/main/java/co/edu/unicauca/componentservice/domain/entity/Component.java
@@ -53,6 +53,38 @@ public class Component implements Serializable{
 		this.type = type;
 	}
 
+	@Override
+	public String toString() {
+		return "Component [id=" + id + ", name=" + name + ", type=" + type + "]";
+	}
+
+	/**
+	 * A specific way to do compare components
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Component other = (Component) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+	
 	/**
 	 * @return the id
 	 */
