99eef74682586aec1a4ecf6ace1bb2a0f9af4c4a
JDMPasquel <jdamupasquel@unicauca.edu.co>
2021-08-17 19:17:40 -0500

    Creating the Dish_management-service

---
+++ dish_management-service/src/main/java/co/edu/unicauca/dish_managementservice/DishManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.dish_managementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishManagementServiceApplication.class, args);
+	}
+
+}
---
+++ dish_management-service/src/test/java/co/edu/unicauca/dish_managementservice/DishManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.dish_managementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
